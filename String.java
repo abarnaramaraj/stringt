11367b33cffe20cd7c8dc3a38116dafeeeb9d454
abarnaramaraj <47477481+abarnaramaraj@users.noreply.github.com>
2019-06-06 13:49:05 +0530

    Create String.java

---
+++ String.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+public class String
+{
+public static void main(String[] args)
+{
+int k,n,i;
+Scanner sc=new Scanner(System.in);
+String b=sc.nextLine();
+char ch[]=b.toCharArray();
+n=b.length();
+for(i=n-k;i<n;i++)
+{
+System.out.print(ch[i]);
+}
+}
+}
+
