d90e379770d8910fc5be2dc7a687cb26b13908bd
abarnaramaraj <47477481+abarnaramaraj@users.noreply.github.com>
2019-06-06 13:57:39 +0530

    Update String.java

--- String.java
+++ String.java
@@ -6,9 +6,10 @@ public static void main(String[] args)
 {
 int k,n,i;
 Scanner sc=new Scanner(System.in);
-String b=sc.nextLine();
+String b=sc.next();
 char ch[]=b.toCharArray();
 n=b.length();
+k=sc.nextInt();
 for(i=n-k;i<n;i++)
 {
 System.out.print(ch[i]);
